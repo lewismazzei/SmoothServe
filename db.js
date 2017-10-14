0676313c9abfd898e61bc1eb21723eaf3ab256b1
lewismazzei <lewis.mazzei@googlemail.com>
2017-10-14 17:12:27 +0200

    update db.js

--- db.js
+++ db.js
@@ -9,7 +9,8 @@ var config = {
 
 //initialise firebase
 firebase.initializeApp(config);
-addOrder(1, 1, [["cocacola",null,3],["fanta","no ice",1]]);
+addOrder("Alex", 1, [["cocacola",null,3],["fanta","no ice",1]]);
+
 
 //initialise database
 var database = firebase.database();
@@ -21,8 +22,8 @@ function addOrder(waiter, table, items) {
     table: table,
     state: 0
   });
-  for (var itemCount = 1; itemCount < items.length+1; itemCount++) {
-  	firebase.database().ref('orders/order-' + orderId + '/item-' + itemCount).set({
+  for (var itemCount = 0; itemCount < items.length; itemCount++) {
+  	firebase.database().ref('orders/order-' + orderId + '/items/item-' + (itemCount + 1)).set({
   		name: items[itemCount][0],
   		notes: items[itemCount][1],
   		quantity: items[itemCount][2]
@@ -30,9 +31,44 @@ function addOrder(waiter, table, items) {
   	console.log("I added an item: " + items[itemCount][0]);
   }
   console.log("I added an order with the id: " + orderId);
+  drawCard(items, orderId);
 }
 
-function addItem(itemName, itemInfo, quantity) {
-	firebase.database().ref('orders/order-')
+/*var orderRef = firebase.database().ref('orders/' + orderId);
+
+starCountRef.on('value', function(snapshot) {
+  updateDrawCard();
+});
+*/
+
+function drawCard(items, orderId) {
+	var card = document.createElement('div');
+	card.id = "order-" + orderId;
+	var cardContent = card.appendChild(document.createElement('div'));
+		var cardHeader = cardContent.appendChild(document.createElement('div'));
+		var cardTime = cardContent.appendChild(document.createElement('div'));
+		var cardDescription = cardContent.appendChild(document.createElement('div'));
+			var cardList = cardDescription.appendChild(document.createElement('div'));
+			for (i = 0; i < items.length; i++) {
+				var listItem += cardlist.appendChild('div')
+				listItem.className("item");
+				var listIcon += listItem.appendChild('i');
+				listIcon.className("icon");
+				
+			}
+	var cardExtraContent = card.appendChild(document.createElement('div'));
+
+	cardContent.className += "content";
+	cardHeader.className +=  "header";
+	cardTime.className += "meta";
+	cardDescription.className += "description";
+	cardList.className += "ui list";
+	cardExtraContent.className += "extra content";
+
+	document.getElementById("ordersDiv").appendChild(card);
+}
+
+function updateCard() {
+
 }
 
