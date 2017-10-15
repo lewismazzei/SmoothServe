b8b7482e5ab7cc2be797dd90f75423905b0d50f1
Alvaro <alvaromr898@gmail.com>
2017-10-15 02:38:18 +0200

    All working + timing things

--- db.js
+++ db.js
@@ -10,16 +10,14 @@ firebase.initializeApp(config);
 ////addOrder("Alex", 1, [{name: "cocacola", quantity: 3}, {name: "espresso", notes: "hot", quantity: 1}]);
 
 var database = firebase.database();
-var totalSeconds = 0;
-var timers = {};
 
 function addOrder(waiter, table, items) {
   database.ref('/orders/').push({
     waiter: waiter,
     table: table,
     state: 0,
-    items: items
-    start_time: new Date().getTime();
+    items: items,
+    startTime: Date.now()
   });
 }
 
@@ -46,44 +44,6 @@ order_ref.on('child_removed', function(data) {
 
 
 function drawCard(items, table, orderId) {
-<<<<<<< HEAD
-	var card = document.createElement('div');
-	card.id = "order-" + orderId;
-	card.className += "card red";
-	var cardContent = card.appendChild(document.createElement('div'));
-		var cardHeader = cardContent.appendChild(document.createElement('div'));
-		var cardTime = cardContent.appendChild(document.createElement('div'));
-		var cardDescription = cardContent.appendChild(document.createElement('div'));
-			var cardList = cardDescription.appendChild(document.createElement('div'));
-			for (itemCount = 0; itemCount < items.length; itemCount++) {
-				var listItem = cardList.appendChild(document.createElement('div'));
-				listItem.id = "item-" + (itemCount+1);
-				listItem.className += "item";
-				var listIcon = listItem.appendChild(document.createElement('i'));
-				listIcon.className += "icon";
-			}
-		var cardExtraContent = card.appendChild(document.createElement('div'));
-		cardExtraContent.innerHTML = '<a class="left floated" onclick="updateStatusToServed(this.parentNode.parentNode.id);"><i class="send icon"></i>Done</a><a class="right floated" onclick="modifyOrder(this.parentNode.parentNode.id);"><i class="edit icon"></i>Modify</a>';
-
-	cardContent.className += "content";
-	cardHeader.className +=  "header";
-	cardTime.className += "meta";
-	cardTime.id += "timer" + orderId;
-	cardDescription.className += "description";
-	cardList.className += "ui list";
-	cardExtraContent.className += "extra content";
-
-	var totalSeconds = 0;
-	document.getElementById("ordersDiv").appendChild(card);
-	cardHeader.textContent = "Table " + table;
-	timer = setInterval ( function(){
-		++totalSeconds;
-		var hours = Math.floor(totalSeconds / 3600);
-		var minutes = Math.floor((totalSeconds - hours * 3600) / 60);
-		var seconds = totalSeconds - (hours * 3600 + minutes * 60);
-	document.getElementById("timer" + orderId).innerHTML = minutes + "min " + seconds + "sec";
-	}, 1000);
-=======
   var card = document.createElement('div');
   card.id = orderId;
   card.className += "card green";
@@ -92,7 +52,6 @@ function drawCard(items, table, orderId) {
   var cardTime = cardContent.appendChild(document.createElement('div'));
   var cardDescription = cardContent.appendChild(document.createElement('div'));
   var cardList = cardDescription.appendChild(document.createElement('div'));
->>>>>>> 1ecae06308680527460e57ba4937e9cbaa82441a
 
   $.each(items, function(key, value) {
     var listItem = cardList.appendChild(document.createElement('div'));
@@ -117,8 +76,7 @@ function drawCard(items, table, orderId) {
   cardContent.className += "content";
   cardHeader.className += "header";
   cardTime.className += "meta";
-  cardTime.id += orderId;
-  timers[cardTime.id] = totalSeconds;
+  cardTime.id = "timer"+orderId;
   cardDescription.className += "description";
   cardList.className += "ui list";
   cardExtraContent.className += "extra content";
@@ -127,89 +85,21 @@ function drawCard(items, table, orderId) {
   cardHeader.textContent = "Table " + table;
 }
 
-function updateCardTimer(timerId, offsetSeconds) {
-  var mySeconds = totalSeconds - offsetSeconds;
-  var minutes = Math.floor(mySeconds / 60);
-  var mySeconds = mySeconds - minutes * 60;
-  var cardClasses = document.getElementById("order-" + timerId.split(5)).classlist;
-  cardClasses = "card";
-  switch (minutes) {
-    case 0:
-      cardClasses += "teal";
-      break;
-    case 1:
-      cardClasses += "green";
-      break;
-    case 2:
-      cardClasses += "olive";
-      break;
-    case 3:
-      cardClasses += "yellow";
-      break;
-    case 4:
-      cardClasses += "orange";
-      break;
-    default:
-      cardClasses += "red";
-      break;
-  }
-  document.getElementById(timerId).innerHTML = minutes + "min " + mySeconds + "sec";
+function updateCardTimer(orderId) {
+  setInterval( function(){
+    var timer = document.getElementById("timer"+orderId);
+    timer.textContent = Date.now() - data.val()
+  }, 1000)
 }
 
 function updateStatusToServed(orderId) {
-<<<<<<< HEAD
-	 clearInterval(timer);
-	 firebase.database().ref('orders/' + orderId).set({
-	 	state: 1
-	 });
-	 var servedCard = document.getElementById(orderId);
-	 servedCard.remove();
-	 return servedCard;
-}
-
-function updateStatusToPayed(orderId) {
-	 firebase.database().ref('orders/' + orderId).set({
-	 	state: 2
-	 });
-	 document.getElementById(orderId).remove();
-=======
   delete timers["timer" + orderId.slice(6)];
   firebase.database().ref('orders/' + orderId).set({
     state: 1
   });
   document.getElementById(orderId).remove();
->>>>>>> 1ecae06308680527460e57ba4937e9cbaa82441a
 }
 
 function modifyOrder() {
 
 }
-
-<<<<<<< HEAD
-//VIEW TRANSITION SCRIPTS
-
-    function waitingButtonClicked() {
-      document.getElementById("startView").style.display = 'none';
-      document.getElementById("waitingView").style.display = 'block';
-      //initialView.style.display = "none";
-      //indexView.style.display = "block";
-    }
-    function barButtonClicked() {
-      document.getElementById("startView").style.display = 'none';
-      document.getElementById("barView").style.display = 'block';
-      //initialView.style.display = "none";
-      //indexView.style.display = "block";
-    }
-    function kitchenButtonClicked() {
-      document.getElementById("startView").style.display = 'none';
-      document.getElementById("kitchenView").style.display = 'block';
-      //initialView.style.display = "none";
-      //indexView.style.display = "block";
-    }
-    document.getElementById("waitingButton").addEventListener("click", waitingButtonClicked);
-    document.getElementById("barButton").addEventListener("click", barButtonClicked);
-    document.getElementById("kitchenButton").addEventListener("click", kitchenButtonClicked);
-=======
-
-
->>>>>>> 1ecae06308680527460e57ba4937e9cbaa82441a
